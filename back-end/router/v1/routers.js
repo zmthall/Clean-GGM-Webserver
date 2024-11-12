ed161e227bd11b24a89f2a1b79969b948b34cbde
zmthall <zmthall@gmail.com>
2024-11-12 08:51:37 -0700

    Basic setup for routers with centralized exporting

---
+++ back-end/router/v1/routers.js
@@ -0,0 +1,12 @@
+import blogRouter from './blogRouter.js';
+import commandRouter from './commandRouter.js';
+import contactRouter from './contactRouter.js';
+import eventRouter from './eventRouter.js';
+// import userRouter from './userRouter.js'; // this router is not implemented yet
+
+export {
+    blogRouter,
+    commandRouter,
+    contactRouter,
+    eventRouter
+};
